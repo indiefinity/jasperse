8354b3ba7ccd9d542666ff59a72631996b0db3cc
jasu06 <53861704+jasu06@users.noreply.github.com>
2019-08-19 16:06:40 +0300

    Tämä on älykästä

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
 function buttonPressed() {
-    document.getElementById('text').innerHTML = "your gey!"
+    document.getElementById('text').innerHTML = "Jos et ole älykäs, et ole tyhmäkään!"
 }
 console.log("haha!")
